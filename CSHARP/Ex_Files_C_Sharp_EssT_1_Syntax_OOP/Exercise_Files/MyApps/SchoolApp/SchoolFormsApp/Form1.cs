188a984845368bd818a2fa7842b6d299e01cdff5
Lubowicki Arkadiusz <Arkadiusz.Lubowicki@gmail.com>
2021-12-29 09:44:04 +0100

    testing_ToString

--- CSHARP/Ex_Files_C_Sharp_EssT_1_Syntax_OOP/Exercise_Files/MyApps/SchoolApp/SchoolFormsApp/Form1.cs
+++ CSHARP/Ex_Files_C_Sharp_EssT_1_Syntax_OOP/Exercise_Files/MyApps/SchoolApp/SchoolFormsApp/Form1.cs
@@ -35,7 +35,8 @@ namespace SchoolFormsApp
             {
                 MessageBox.Show(ex.Message);
             }
-            
+
+            MessageBox.Show(testSchool.ToString());
         }
     }
 }
