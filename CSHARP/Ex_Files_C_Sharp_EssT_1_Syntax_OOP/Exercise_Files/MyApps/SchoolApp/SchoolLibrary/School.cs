30da60471777b5661be2fb44e29d4981b7cd468d
Lubowicki Arkadiusz <Arkadiusz.Lubowicki@gmail.com>
2021-12-29 08:21:48 +0100

    Creating classes

---
+++ CSHARP/Ex_Files_C_Sharp_EssT_1_Syntax_OOP/Exercise_Files/MyApps/SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneNumber;
+
+
+    }
+}
